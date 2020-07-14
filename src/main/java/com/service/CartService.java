1b569a1623e44069b1ac71be2f2bd1a10c01ccf5
YutaSawagami <yuta.sawagami@rakus-partners.co.jp>
2020-07-14 16:40:07 +0900

    create cart

--- src/main/java/com/service/CartService.java
+++ src/main/java/com/service/CartService.java
@@ -1,4 +1,29 @@
 package com.service;
 
+import com.domain.Cart;
+import com.domain.Item;
+import com.domain.OrderItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Service
 public class CartService {
-}
+
+    @Autowired
+    private HttpSession session;
+
+    public Cart addCart(Item item) {
+        List<OrderItem> cartInScope = (List<OrderItem>) session.getAttribute("cart");
+        if (cart == null) {
+            Cart cart = new Cart();
+
+        }
+        for (OrderItem orderItem : cart) {
+
+
+        }return cart;
+    }
+

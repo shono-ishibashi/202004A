5576f7110a1be6e3f5130903765255d640993584
YutaSawagami <yuta.sawagami@rakus-partners.co.jp>
2020-07-15 15:25:52 +0900

    create validationCheck for resist user

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.form.UserForm;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -35,22 +36,38 @@ public class LoginController {
     @RequestMapping("/register-user/insert")
     private String insertUser(
             @Validated UserForm userForm,
-            BindingResult retult
+            BindingResult result,
+            Model model
             ){
 
-        if(retult.hasErrors()){
+        System.out.println(userForm);
+
+        if(!userForm.getPassword().equals(userForm.getConfirmationPassword())){
+            result.rejectValue("password", "", "パスワードが一致していません");
+            result.rejectValue("confirmationPassword", "", "");
+
+        }
+
+        User existUser = userService.findUserByEmail(userForm.getEmail());
+        if(existUser != null){
+            result.rejectValue("email", "", "そのメールアドレスは既に登録されています");
+        }
+
+        if(result.hasErrors()){
             return showInsertUser();
         }
+
+        String zipCode = userForm.getZipcode().replace("-","");
 
         User user = new User();
         user.setName(userForm.getName());
         user.setPassword(userForm.getPassword());
         user.setEmail(userForm.getEmail());
         user.setAddress(userForm.getAddress());
-        user.setZipcode(userForm.getZipcode());
+        user.setZipcode(zipCode);
         user.setTelephone(userForm.getTelephone());
 
         userService.insertUser(user);
-        return "redirect:login";
+        return showForm();
     }
 }
